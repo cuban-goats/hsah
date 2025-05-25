c4e9db73eac2f80dc882e4a6879d5db74334c721
cuban-goats <erikkoston@mailfence.com>
2025-05-25 16:36:32 +0200

    created binaryConversion() function

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("hehe");
+    String input = "This will soon be a hash";
+    String result = binary_conversion(input);
+    System.out.println(result);
+  }
+
+  public static String binary_conversion(String input) {
+    StringBuilder result = new StringBuilder();
+    char[] chars = input.toCharArray();
+
+    for (char aCHar : chars) {
+      result.append(String.format("%8s", Integer.toBinaryString(aCHar)).replaceAll(" ", "0"));
+    }
+    return result.toString();
   }
 }
