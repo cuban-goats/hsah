f47fdf5e4403fcdce1d86a7878f4e096ab2e53ff
cuban-goats <erikkoston@mailfence.com>
2025-05-25 17:29:06 +0200

    added size adjustment function

--- Main.java
+++ Main.java
@@ -1,17 +1,28 @@
 public class Main {
   public static void main(String[] args) {
-    String input = "This will soon be a hash";
-    String result = binary_conversion(input);
-    System.out.println(result);
+    String converted = binary_conversion("Hello");
+    String adjusted = adjust_size(converted);
+    System.out.println(adjusted);
+
   }
 
   public static String binary_conversion(String input) {
     StringBuilder result = new StringBuilder();
     char[] chars = input.toCharArray();
-
     for (char aCHar : chars) {
       result.append(String.format("%8s", Integer.toBinaryString(aCHar)).replaceAll(" ", "0"));
     }
     return result.toString();
+  }
+
+  public static String adjust_size(String converted) {
+    StringBuilder adjusted = new StringBuilder().append(converted);
+    if (converted.length() < 64) {
+      while (adjusted.length() < 64) {
+        adjusted.append("0");
+      }
+
+    }
+    return adjusted.toString();
   }
 }
