cc215983a8b912648a1b751569373305063314c7
cuban-goats <erikkoston@mailfence.com>
2025-06-11 16:30:27 +0200

    created a trng and prng package

---
+++ prng/PrngClass.java
@@ -0,0 +1,10 @@
+package prng;
+
+import crypt.HsahingClass;
+
+public class PrngClass {
+  public static void main(String[] args) {
+    System.out.println("Soon a Prng");
+  }
+}
+
---
+++ trng/TrngClass.java
@@ -0,0 +1,9 @@
+package trng;
+
+import crypt.HsahingClass;
+
+public class TrngClass {
+  public static void main(String[] args) {
+    System.out.println("Soon a trng");
+  }
+}
